7658ce3b3e6b6007e6c9e6502ae0e1fc97433817
acikek <spiralixp@gmail.com>
2022-04-22 12:03:41 -0700

    attempt 2

--- src/main/java/com/acikek/voidcrafting/compat/rei/VoidCraftingREIPlugin.java
+++ src/main/java/com/acikek/voidcrafting/compat/rei/VoidCraftingREIPlugin.java
@@ -5,11 +5,12 @@ import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
 import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
 
-public class VoidCraftingClientPlugin implements REIClientPlugin {
+public class VoidCraftingREIPlugin implements REIClientPlugin {
 
     @Override
     public void registerCategories(CategoryRegistry registry) {
-        registry.add(new VoidRecipeDisplayCategory());
+        registry.add(new VoidRecipeCategory());
+        registry.addWorkstations(VoidRecipeDisplay.IDENTIFIER, VoidRecipeCategory.ICON);
     }
 
     @Override
--- src/main/java/com/acikek/voidcrafting/compat/rei/VoidRecipeDisplay.java
+++ src/main/java/com/acikek/voidcrafting/compat/rei/VoidRecipeDisplay.java
@@ -4,14 +4,9 @@ import com.acikek.voidcrafting.VoidCrafting;
 import com.acikek.voidcrafting.recipe.VoidRecipe;
 import me.shedaniel.rei.api.common.category.CategoryIdentifier;
 import me.shedaniel.rei.api.common.display.Display;
-import me.shedaniel.rei.api.common.display.basic.BasicDisplay;
 import me.shedaniel.rei.api.common.entry.EntryIngredient;
-import me.shedaniel.rei.api.common.util.CollectionUtils;
 import me.shedaniel.rei.api.common.util.EntryIngredients;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NbtCompound;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -22,34 +17,28 @@ public class VoidRecipeDisplay implements Display {
     /*public float radius;
     public float x;
     public float y;*/
-    public List<EntryIngredient> inputs;
-    public List<EntryIngredient> outputs;
+    public List<EntryIngredient> input;
+    public List<EntryIngredient> output;
     public boolean absolute;
 
     public VoidRecipeDisplay(VoidRecipe recipe) {
-        inputs = Collections.singletonList(EntryIngredients.of(recipe.input().getMatchingStacks()[0])); //CollectionUtils.map(recipe.input().getMatchingStacks(), EntryIngredients::of);
-        outputs = Collections.singletonList(EntryIngredients.of(recipe.getOutput()));
+        input = Collections.singletonList(EntryIngredients.ofIngredient(recipe.input())); //CollectionUtils.map(recipe.input().getMatchingStacks(), EntryIngredients::of);
+        output = Collections.singletonList(EntryIngredients.of(recipe.getOutput()));
         absolute = recipe.absolute();
     }
 
     @Override
     public List<EntryIngredient> getInputEntries() {
-        return inputs;
+        return input;
     }
 
     @Override
     public List<EntryIngredient> getOutputEntries() {
-        return outputs;
+        return output;
     }
 
     @Override
     public CategoryIdentifier<?> getCategoryIdentifier() {
         return IDENTIFIER;
     }
-
-    /*public static BasicDisplay.Serializer<VoidRecipeDisplay> getSerializer() {
-        return BasicDisplay.Serializer.ofRecipeLess(VoidRecipeDisplay::new, (display, nbt) -> {
-            nbt.putBoolean("Absolute", display.absolute);
-        });
-    }*/
 }
