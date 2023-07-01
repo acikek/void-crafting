e59fb5affbbe153f4c8ffb26f9ac5dad29a4a9df
acikek <spiralixp@gmail.com>
2023-06-30 18:00:39 -0700

    1.20 port

--- src/main/java/com/acikek/voidcrafting/compat/emi/VoidCraftingPlugin.java
+++ src/main/java/com/acikek/voidcrafting/compat/emi/VoidCraftingPlugin.java
@@ -2,22 +2,18 @@ package com.acikek.voidcrafting.compat.emi;
 
 import com.acikek.voidcrafting.VoidCrafting;
 import com.acikek.voidcrafting.recipe.VoidRecipe;
-import com.mojang.blaze3d.systems.RenderSystem;
 import dev.emi.emi.api.EmiPlugin;
 import dev.emi.emi.api.EmiRegistry;
 import dev.emi.emi.api.recipe.EmiRecipeCategory;
 import dev.emi.emi.api.render.EmiRenderable;
-import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.util.Identifier;
 
 public class VoidCraftingPlugin implements EmiPlugin {
 
     public static final Identifier WIDGETS = VoidCrafting.id("textures/gui/widgets.png");
 
-    public static final EmiRenderable CATEGORY_RENDERER = (matrices, x, y, delta) -> {
-        RenderSystem.setShaderTexture(0, WIDGETS);
-        DrawableHelper.drawTexture(matrices, x, y, 78, 0, 16, 16, 256, 256);
-    };
+    public static final EmiRenderable CATEGORY_RENDERER = (ctx, x, y, delta) ->
+        ctx.drawTexture(WIDGETS, x, y, 78, 0, 16, 16, 256, 256);
 
     public static EmiRecipeCategory VOID_CRAFTING = new EmiRecipeCategory(
             VoidCrafting.id("void_crafting"),
